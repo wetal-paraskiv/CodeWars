252040493e6bb21223285c085171a3792015da7b
Paraschiv <vparasch@techteam.com>
2023-09-28 17:01:39 +0300

    ...

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,24 +1,14 @@
-import java.util.ArrayList;
 
 public class Test {
    // https://www.indiabix.com/online-test/java-programming-test/
 
-
-
    public static void main(String[] args) {
-
-      Boolean b1 = new Boolean("TRUE");
-      Boolean b2 = new Boolean("true");
-      Boolean b3 = new Boolean("tRuE");
-      Boolean b4 = new Boolean("false");
-      System.out.println(b4);
+      String x = "xyz";
+      x = x + "qqq";
+      x.toUpperCase(); /* Line 2 */
+      String y = x;
+      y = y + "abc";
+      System.out.println(y);
    }
 }
 
-
-interface Base
-{
-   boolean m1 ();
-   byte m2(short s);
-}
-abstract class Class2 implements Base {}
